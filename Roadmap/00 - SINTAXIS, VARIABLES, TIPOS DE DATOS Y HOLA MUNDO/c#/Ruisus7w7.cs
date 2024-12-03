19a3fc1703522910791d7777f0bc5c29ea651d76
Ruisus7w7 <ruisusrojas@gmail.com>
2024-12-03 15:32:08 -0500

    Ruisus7w7.cs

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/Ruisus7w7.cs
@@ -0,0 +1,33 @@
+// https://learn.microsoft.com/en-us/dotnet/csharp/
+
+/*
+Esto es un comentario 
+en varias lineas
+*/
+
+var variable = 5;
+
+const int constante = 10;
+
+int intnumber = 20; 
+float number = 1.75;
+double float nuber = 2.0;
+char letra = 'A';
+string nombre = "jose";
+
+Bool esverdadero = true;
+bool esfalso = false;
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Nombre del lenguaje
+        string lenguaje = "C#";
+
+        // Imprimir el mensaje
+        Console.WriteLine($"¡Hola, {lenguaje}!");
+    }
+}
